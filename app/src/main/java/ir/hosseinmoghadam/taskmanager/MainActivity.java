b6a416bd6c5e2094611a2d77f32d741147f897dc
hossein moghadam <hosseinghmoghadam@gmail.com>
2018-04-14 10:04:51 +0430

    add logout

--- app/src/main/java/ir/hosseinmoghadam/taskmanager/MainActivity.java
+++ app/src/main/java/ir/hosseinmoghadam/taskmanager/MainActivity.java
@@ -1,5 +1,6 @@
 package ir.hosseinmoghadam.taskmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -114,6 +115,8 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
             return true;
         }
 
